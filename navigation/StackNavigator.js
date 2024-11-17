8e252e82597210a2577f5ee9bb2b82f49123d34f
Can Gündüz <cangunduz0001@gmail.com>
2024-11-17 23:23:34 +0300

    1.0.1

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,8 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import EventScreen from '../screens/EventScreen';
 import AdminDashboard from '../screens/admin/AdminDashboard';
-import AdminEventScreen from '../screens/admin/AdminEventScreen'; // Import the new screen
-import BookScreen from '../screens/BookScreen';
+import AdminEventScreen from '../screens/admin/AdminEventScreen'; 
 import {NavigationContainer} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -17,38 +16,25 @@ import NameScreen from '../screens/NameScreen';
 import SelectImage from '../screens/SelectImage';
 import PreFinalScreen from '../screens/PreFinalScreen';
 import {AuthContext} from '../AuthContext';
-import CreateEvent from '../screens/CreateEvent';
 import TagVenueScreen from '../screens/TagVenueScreen';
 import SelectTimeScreen from '../screens/SelectTimeScreen';
 import EventSetUpScreen from '../screens/EventSetUpScreen';
 import AttendeesScreen from '../screens/admin/AttendeesScreen';
 import ManageRequests from '../screens/admin/ManageRequest';
 import ProfileDetailScreen from '../screens/ProfileDetail';
-import ChatsScreen from '../screens/ChatsScreen';
-import RequestChatRoom from '../screens/RequestChatRoom';
-import ChatRoom from '../screens/ChatRoom';
 import PeopleScreen from '../screens/PeopleScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import AdminCreateScreen from '../screens/admin/AdminCreateScreen';
 import HomeScreen from '../screens/HomeScreen';
 import AdminEventSetUpScreen from '../screens/admin/AdminEventSetUpScreen';
 import ProfileEditScreen from '../screens/ProfileEditScreen';
-import TicketDetailScreen from '../screens/TicketDetailScreen';
 import AdminCreateVenueScreen from '../screens/admin/AdminCreateVenueScreen';
 import InterestSelectionScreen from '../screens/InterestSelectionScreen';
 import SearchScreen from '../screens/SearchScreen';
-import ReviewScreen from '../screens/ReviewScreen';
 import NotificationScreen from '../screens/NotificationScreen';
 import ProfileViewScreen from '../screens/ProfileViewScreen';
 import EventAttendeesScreen from '../screens/EventAttendeesScreen';
-import CustomDrawerNavigator from './CustomDrawerNavigator';
-import AdminCreateCommunityScreen from '../screens/admin/AdminCreateCommunityScreen';
-import CommunityScreen from '../screens/CommunityScreen';
-import CommunityDetailScreen from '../screens/CommunityDetailScreen';
-import AdminManageCommunityScreen from '../screens/admin/AdminManageCommunityScreen';
-import AdminCommunityScreen from '../screens/admin/AdminCommunityScreen';
-import AdminCommunityDetailScreen from '../screens/admin/AdminCommunityDetailScreen';
-import PostScreen from '../screens/PostScreen';
+
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
@@ -199,18 +185,8 @@ const StackNavigator = () => {
               component={AdminEventSetUpScreen}
               options={{headerShown: false}}
             />
-            <Stack.Screen
-              name="AdminCommunity"
-              component={AdminCreateCommunityScreen}
-              options={{headerShown: false}}
-            />
           </>
         )}
-        <Stack.Screen
-          name="Community"
-          component={CommunityScreen}
-          options={{headerShown: false}}
-        />
         <Stack.Screen
           name="TagVenue"
           component={TagVenueScreen}
@@ -248,24 +224,13 @@ const StackNavigator = () => {
           component={EventScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen
-          name="AdminManageCommunityScreen"
-          component={AdminManageCommunityScreen}
-          options={{headerShown: false}}
-        />
+        
         <Stack.Screen
           name="EventSetUp"
           component={EventSetUpScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen name="Request" component={RequestChatRoom} />
-        <Stack.Screen name="ChatRoom" component={ChatRoom} />
-        <Stack.Screen name="Chats" component={ChatsScreen} />
         <Stack.Screen name="ProfileEditScreen" component={ProfileEditScreen} />
-        <Stack.Screen
-          name="TicketDetailScreen"
-          component={TicketDetailScreen}
-        />
         <Stack.Screen
           name="AdminCreateVenue"
           component={AdminCreateVenueScreen}
@@ -280,28 +245,8 @@ const StackNavigator = () => {
           options={{headerShown: false}}
         />
         <Stack.Screen
-          name="ReviewScreen"
-          component={ReviewScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="PostScreen"
-          component={PostScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
           name="NotificationScreen"
           component={NotificationScreen}
-          options={{headerShadowVisible: false}}
-        />
-        <Stack.Screen
-          name="CommunityScreen"
-          component={CommunityScreen}
-          options={{headerShadowVisible: false}}
-        />
-        <Stack.Screen
-          name="AdminCreateCommunity"
-          component={AdminCreateCommunityScreen}
           options={{headerShadowVisible: false}}
         />
         <Stack.Screen
@@ -312,21 +257,6 @@ const StackNavigator = () => {
         <Stack.Screen
           name="EventAttendees"
           component={EventAttendeesScreen}
-          options={{headerShadowVisible: false}}
-        />
-        <Stack.Screen
-          name="CommunityDetailScreen"
-          component={CommunityDetailScreen}
-          options={{headerShadowVisible: false}}
-        />
-        <Stack.Screen
-          name="AdminCommunityScreen"
-          component={AdminCommunityScreen}
-          options={{headerShadowVisible: false}}
-        />
-        <Stack.Screen
-          name="AdminCommunityDetailScreen"
-          component={AdminCommunityDetailScreen}
           options={{headerShadowVisible: false}}
         />
         <Stack.Screen
